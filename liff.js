01f055ea4ccf99c01882774eaf1ae99f2e2f7dad
kakotetsuoml <114625062+kakotetsuoml@users.noreply.github.com>
2022-10-02 14:49:45 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657521349-Ww6D6YKZ";
+    var liffId = "1657521469-vL7W7d8p";
     initializeLiff(liffId);
 })
 
