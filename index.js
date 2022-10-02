88de33cdc5947e5b7bcb405053cb1c75570b745e
kakotetsuoml <114625062+kakotetsuoml@users.noreply.github.com>
2022-10-02 14:21:55 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+$(function () {
+    // 送信
+    $('form').submit(function () {
+        var name = $('input[name="name"]').val();
+        var kyojuuku = $('input[name="kyojuuku"]').val();  
+        var title = $('input[name="title"]').val();
+        var content = $('input[name="content"]').val();
+        var msg = `氏名：${name}\n居住区：${kyojuuku}\n件名：${title}\n内容：${content}`;
+        sendText(msg);
+
+        return false;
+    });
+});
