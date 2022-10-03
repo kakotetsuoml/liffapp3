dd3704eb964c81bf7ba8c9dd5a09915d6ebbd4a1
kakotetsuoml <114625062+kakotetsuoml@users.noreply.github.com>
2022-10-03 21:50:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function () {
         var name = $('input[name="name"]').val();
         var kyojuuku = $('input[name="kyojuuku"]').val();  
         var title = $('input[name="title"]').val();
-        var content = document.getElementById( "message" ).val(); 
+        var content = $('document.getElementById( "message" )).val(); 
         var msg = `氏名：${name}\n居住区：${kyojuuku}\n件名：${title}\n内容：${content}`;
         sendText(msg);
 
